c6fc459553c62d51098abeea949b8192b4508b70
Akash Singh <akashrautana01@gmail.com>
2026-09-06 21:50:54 +0530

    Time: 9 ms (99.67%) | Memory: 82.1 MB (99.72%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if (nums.length < 2) return 0;
+        
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        if (min == max) return 0;
+
+        int n = nums.length;
+        int bucketSize = (int)Math.ceil((double)(max - min) / (n - 1));
+        int bucketCount = (max - min) / bucketSize + 1;
+
+        int[] bucketMin = new int[bucketCount];
+        int[] bucketMax = new int[bucketCount];
+        Arrays.fill(bucketMin, Integer.MAX_VALUE);
+        Arrays.fill(bucketMax, Integer.MIN_VALUE);
+
+        for (int num : nums) {
+            int idx = (num - min) / bucketSize;
+            bucketMin[idx] = Math.min(bucketMin[idx], num);
+            bucketMax[idx] = Math.max(bucketMax[idx], num);
+        }
+
+        int maxGap = 0, prev = min;
+        for (int i = 0; i < bucketCount; i++) {
+            if (bucketMin[i] == Integer.MAX_VALUE) continue; // empty bucket
+            maxGap = Math.max(maxGap, bucketMin[i] - prev);
+            prev = bucketMax[i];
+        }
+        return maxGap;
+    }
+}
